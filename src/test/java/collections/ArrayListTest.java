213db0d6717ab7b0adea327b0cb0dae9d6fdb58b
Joker-5 <2725284321@qq.com>
2021-11-30 20:41:15 +0800

    feat: make ArrayList analyse entrance in order

--- src/test/java/collections/ArrayListTest.java
+++ src/test/java/collections/ArrayListTest.java
@@ -5,13 +5,15 @@ import java.util.Arrays;
 
 public class ArrayListTest {
     public static void main(String[] args) {
+        // 1.含参构造器分析
         ArrayList<Integer> list = new ArrayList<>(10);
+        // 2.add方法分析
         list.add(1);
+        // 3.指定idx的add方法分析
         list.add(1, 11);
-        list.hashCode();
+        // 4.toArray方法分析
         Object[] array = list.toArray();
-        Arrays.stream(array).forEach(e -> System.out.println(e));
+        // 5.ensureCapacity方法分析
         list.ensureCapacity(80);
-        System.out.println(list.size());
     }
 }
